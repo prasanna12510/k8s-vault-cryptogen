f4f51afccfbdfa28035fe39f9c11f5dc396200af
prasanna2705 <pawar.prasanna@active.ai>
2019-05-06 13:49:02 +0800

    develop cryptogenwebservice

--- worker.py
+++ worker.py
@@ -5,12 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDIS_URL', 'vaultstore.redis.cache.windows.net')
-redis_token = os.getenv('REDIS_TOKEN','SoofWT5O2ZS88+MirB5ltw7fLzrysn+3PhCdHshnpfo=')
+redis_url = os.getenv('REDIS_HOST', 'vaultstore.redis.cache.windows.net')
 redis_port = os.getenv('REDIS_PORT','6379')
 
 
-conn = redis.Redis(host=redis_url,port=redis_port,password=redis_token)
+conn = redis.Redis(host=redis_url,port=redis_port)
 
 if __name__ == '__main__':
     with Connection(conn):
